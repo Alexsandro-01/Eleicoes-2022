92c6fb4d1c7ac5b053dd19b8e0b2648c16f1f738
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-10-30 12:30:11 -0300

    feat: renderização dos cards dos candidatos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,17 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
+import CandidatoCard from './CandidatoCard';
+import '../styles/main.css';
+
 
 function Main() {
-  const [cand, setCand] = useState([]);
+  const [candidatos, setCandidatos] = useState([]);
 
   async function fetchData() {
     const URL = 'https://resultados.tse.jus.br/oficial/ele2022/545/dados-simplificados/br/br-c0001-e000545-r.json'
     const { data } = await axios.get(URL);
     
-    setCand(data.cand);
+    setCandidatos(data.cand);
   }
 
   useEffect(() => {
@@ -16,8 +19,12 @@ function Main() {
   }, [])
 
   return (
-    <main>
-      <p>helo</p>
+    <main className='container'>
+      {
+        candidatos.length > 0 && (
+          candidatos.map((cand) => <CandidatoCard key={cand.n} candData={ cand } /> )
+        )
+      }
     </main>
   )
 }
